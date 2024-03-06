eadcbd1de4eef1f8bfe2e0d8d60c56dcb3cc7b90
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-03-06 13:16:25 +0000

    Convert to cupertino

--- lib/my_homepage.dart
+++ lib/my_homepage.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:mini_solo/views/clue_view.dart';
 import 'package:mini_solo/views/dice_view.dart';
 import 'package:mini_solo/views/faction_view.dart';
@@ -10,80 +10,6 @@ import 'package:mini_solo/widgets/random_buttons.dart';
 import 'package:mini_solo/widgets/dice_bar.dart';
 import 'package:mini_solo/widgets/output.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _currentPageIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 0.0,
-          horizontal: 8.0,
-        ),
-        child: NavigationBar(
-          onDestinationSelected: (int index) {
-            setState(() {
-              _currentPageIndex = index;
-            });
-          },
-          indicatorColor: Colors.amber,
-          selectedIndex: _currentPageIndex,
-          destinations: const [
-            NavigationDestination(icon: Icon(Icons.person), label: 'Person'),
-            NavigationDestination(icon: Icon(Icons.place), label: 'Place'),
-            NavigationDestination(
-                icon: Icon(Icons.emoji_objects), label: 'Thing'),
-            NavigationDestination(icon: Icon(Icons.group), label: 'Faction'),
-            NavigationDestination(icon: Icon(Icons.zoom_in), label: 'Clue'),
-            VerticalDivider(),
-            NavigationDestination(icon: Icon(Icons.notes), label: 'Lists'),
-            NavigationDestination(icon: Icon(Icons.square), label: 'Dice'),
-          ],
-        ),
-      ),
-      body: [
-        const PeopleView(),
-        const PlaceView(),
-        const ThingView(),
-        const FactionView(),
-        const ClueView(),
-        const DividerView(),
-        const ListsView(),
-        const DiceView(),
-      ][_currentPageIndex],
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const Column(children: [
-      Output(),
-      RandomButtons(),
-      DiceBar(),
-    ]);
-  }
-}
-
 class DividerView extends StatelessWidget {
   const DividerView({super.key});
 
@@ -92,4 +18,72 @@ class DividerView extends StatelessWidget {
     // TODO: implement build
     throw UnimplementedError();
   }
+}
+
+class MyHomePageIOS extends StatefulWidget {
+  const MyHomePageIOS({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  State<MyHomePageIOS> createState() => _MyHomePageIOSState();
+}
+
+class _MyHomePageIOSState extends State<MyHomePageIOS> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      backgroundColor: CupertinoColors.systemGreen,
+      tabBar: CupertinoTabBar(
+        items: const [
+          BottomNavigationBarItem(
+              label: 'Person', icon: Icon(CupertinoIcons.person)),
+          BottomNavigationBarItem(
+              label: 'Place', icon: Icon(CupertinoIcons.placemark)),
+          BottomNavigationBarItem(
+              label: 'Thing', icon: Icon(CupertinoIcons.bag)),
+          BottomNavigationBarItem(
+              label: 'Faction', icon: Icon(CupertinoIcons.group)),
+          BottomNavigationBarItem(
+              label: 'Clue', icon: Icon(CupertinoIcons.zoom_in)),
+          BottomNavigationBarItem(
+              label: 'Lists', icon: Icon(CupertinoIcons.pen)),
+          BottomNavigationBarItem(
+              label: 'Dice', icon: Icon(CupertinoIcons.cube)),
+        ],
+      ),
+      tabBuilder: (BuildContext context, int index) {
+        return CupertinoTabView(
+          builder: (BuildContext context) {
+            return CupertinoPageScaffold(
+              navigationBar: const CupertinoNavigationBar(
+                middle: Text('Solo app'),
+              ),
+              child: Center(
+                child: CupertinoButton(
+                  onPressed: handleSettingsPressed,
+                  child: [
+                    const PersonView(),
+                    const PlaceView(),
+                    const ThingView(),
+                    const FactionView(),
+                    const ClueView(),
+                    const ListsView(),
+                    const DiceView(),
+                  ][index],
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+void handleSettingsPressed() {
+  print('pressed');
 }
--- lib/views/person_view.dart
+++ lib/views/person_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class PeopleView extends StatelessWidget {
-  const PeopleView({super.key});
+class PersonView extends StatelessWidget {
+  const PersonView({super.key});
 
   @override
   Widget build(BuildContext context) {
