18dd2f33266366ba2878c93e9abbd1b6e6b559f4
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-03-08 17:15:58 +0000

    Add popup for button menus

--- lib/views/new_view.dart
+++ lib/views/new_view.dart
@@ -8,16 +8,57 @@ class NewView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ViewWrapper(children: [
-      ListButton(label: 'Scene', onPressed: doSomething),
-      ListButton(label: 'Person', onPressed: doSomething),
-      ListButton(label: 'Place', onPressed: doSomething),
-      ListButton(label: 'Thing', onPressed: doSomething),
-      ListButton(label: 'Faction', onPressed: doSomething),
-      ListButton(label: 'Clue', onPressed: doSomething),
-      ListButton(label: 'Creature', onPressed: doSomething),
-      ListButton(label: '5 Room Dungeon', onPressed: doSomething),
+    return ViewWrapper(children: [
+      ListButton(
+          label: 'Scene',
+          onPressed: () {
+            buildShowCupertinoModalPopup(context, const NewSceneMenu());
+          }),
+      const ListButton(label: 'Person', onPressed: doSomething),
+      const ListButton(label: 'Place', onPressed: doSomething),
+      const ListButton(label: 'Thing', onPressed: doSomething),
+      const ListButton(label: 'Faction', onPressed: doSomething),
+      const ListButton(label: 'Clue', onPressed: doSomething),
+      const ListButton(label: 'Creature', onPressed: doSomething),
+      const ListButton(label: '5 Room Dungeon', onPressed: doSomething),
     ]);
+  }
+
+  Future<dynamic> buildShowCupertinoModalPopup(
+      BuildContext context, Widget child) {
+    return showCupertinoModalPopup(
+        useRootNavigator: false,
+        context: context,
+        builder: (BuildContext builder) {
+          return CupertinoPopupSurface(
+            child: Container(
+              color: CupertinoColors.white,
+              alignment: Alignment.center,
+              width: double.infinity,
+              // height: 400,
+              child: SafeArea(
+                child: Column(children: [
+                  CupertinoButton(
+                    child: const Text('HI'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  child,
+                ]),
+              ),
+            ),
+          );
+        });
+  }
+}
+
+class NewSceneMenu extends StatelessWidget {
+  const NewSceneMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Passed in Widget');
   }
 }
 
