6df01049e83cd0975c798f761b1f2b48c3765fbb
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-03-19 14:24:25 +0000

    Some tidy up

--- lib/views/new/new_scene.dart
+++ lib/views/new/new_scene.dart
@@ -1,10 +1,7 @@
 import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
-import 'package:provider/provider.dart';
-
 import '../../utilities/read_json_file.dart';
-import '../../widgets/chaos_factor.dart';
+import '../../widgets/chaos_factor_panel.dart';
 import '../../widgets/list_button.dart';
 import '../../widgets/output.dart';
 import '../../widgets/view_wrapper.dart';
@@ -119,62 +116,6 @@ class _NewSceneMenuState extends State<NewSceneMenu> {
         );
       });
     });
-  }
-}
-
-class ChaosFactorPanel extends StatelessWidget {
-  const ChaosFactorPanel({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text('Chaos Factor'),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Expanded(
-              child: ListButton(
-                labelAlignment: Alignment.center,
-                label: 'Up',
-                onPressed: () {
-                  var chaosFactor = context.read<ChaosFactor>();
-                  chaosFactor.increase();
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 8.0),
-              child: Consumer<ChaosFactor>(
-                builder: (context, chaosFactor, child) => SizedBox(
-                  width: 30.0,
-                  child: Center(
-                    child: Text(
-                      chaosFactor.chaosFactor.toString(),
-                      style: const TextStyle(
-                          fontSize: 36.0, color: CupertinoColors.systemPink),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: ListButton(
-                labelAlignment: Alignment.center,
-                label: 'Down',
-                onPressed: () {
-                  var chaosFactor = context.read<ChaosFactor>();
-                  chaosFactor.decrease();
-                },
-              ),
-            ),
-          ],
-        ),
-      ],
-    );
   }
 }
 
