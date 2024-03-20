aeca4ab3c5c8c5ffc256f298927c0b6cc0816938
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-03-20 13:00:05 +0000

    Add close button to all popups

--- lib/widgets/popup.dart
+++ lib/widgets/popup.dart
@@ -7,17 +7,34 @@ Map<PopupLabels, Widget> popupContentMap = {
   PopupLabels.chaos: const ChaosFactorPopup(),
   PopupLabels.fate: const Text('FATE'),
   PopupLabels.endScene: const Text('END SCENE'),
+  PopupLabels.combat: const Text('COMBAT'),
+  PopupLabels.social: const Text('SOCIAL'),
+  PopupLabels.exploration: const Text('EXPLORATION'),
+  PopupLabels.travel: const Text('TRAVEL'),
+  PopupLabels.investigation: const Text('INVESTIGATE'),
 };
 
-Visibility popup(BuildContext context) {
-  var appState = context.read<AppState>();
-  return Visibility(
-    visible: appState.showPopup,
-    child: CupertinoPopupSurface(
-      // child: Center(child: child),
-      child: Center(
-        child: popupContentMap[appState.popupLabel],
-      ),
-    ),
+Consumer<Object?> popup(BuildContext context) {
+  return Consumer<AppState>(
+    builder: (BuildContext context, appState, Widget? child) {
+      return Visibility(
+        visible: appState.showPopup,
+        child: CupertinoPopupSurface(
+          // child: Center(child: child),
+          child: Center(
+            child: Column(
+              children: [
+                CupertinoButton(
+                    child: const Text('Close'),
+                    onPressed: () {
+                      appState.closePopup();
+                    }),
+                popupContentMap[appState.popupLabel] ?? const SizedBox.shrink(),
+              ],
+            ),
+          ),
+        ),
+      );
+    },
   );
 }
