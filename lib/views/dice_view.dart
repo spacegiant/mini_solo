dd891db81f42fdbd30c7a0f4a245b3dd5e097ebf
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-03-24 17:01:17 +0000

    Refactor dice to roll to journal

--- lib/views/dice_view.dart
+++ lib/views/dice_view.dart
@@ -1,8 +1,95 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:mini_solo/widgets/output.dart';
 import 'package:mini_solo/widgets/view_wrapper.dart';
 
 import '../widgets/gap.dart';
+
+class DieType {
+  late Color? color;
+  late String label;
+  late List<DiceFace> faces;
+
+  DieType({
+    this.color,
+    required this.label,
+    required this.faces,
+  });
+
+  int roll() {
+    int numberOfSides = faces.length;
+    int randomRoll = Random().nextInt(numberOfSides) + 1;
+    return randomRoll;
+  }
+}
+
+class DiceFace {
+  late int value;
+  late String? label;
+  late Icon? icon;
+
+  DiceFace({
+    required this.value,
+    this.label,
+    this.icon,
+  });
+}
+
+DieType d4 = DieType(
+  label: 'd4',
+  faces: createDice(4),
+);
+
+DieType d6 = DieType(
+  label: 'd6',
+  faces: createDice(6),
+);
+
+DieType d8 = DieType(
+  label: 'd8',
+  faces: createDice(8),
+);
+
+DieType d10 = DieType(
+  label: 'd10',
+  faces: createDice(10),
+);
+
+DieType d12 = DieType(
+  label: 'd12',
+  faces: createDice(12),
+);
+
+DieType d20 = DieType(
+  label: 'd20',
+  faces: createDice(20),
+);
+
+DieType d100 = DieType(
+  label: 'd100',
+  faces: createDice(100),
+);
+
+List<DiceFace> createDice(int numberOfSides) {
+  return List.generate(numberOfSides,
+      (index) => DiceFace(value: index, label: index.toString()));
+}
+
+class DiceSet {
+  List<DieType> dieTypes;
+  String label;
+
+  DiceSet({
+    required this.dieTypes,
+    required this.label,
+  });
+}
+
+DiceSet regularDice = DiceSet(
+  dieTypes: [d4, d6, d8, d10, d12, d20, d100],
+  label: 'Regular Dice Set',
+);
 
 class DiceView extends StatelessWidget {
   const DiceView({super.key});
@@ -32,6 +119,10 @@ class DiceCollection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Iterable diceButtons = regularDice.dieTypes.map((dieType) => DiceButton(
+          dieType: dieType,
+        ));
+
     return Container(
       padding: const EdgeInsets.all(8.0),
       decoration: BoxDecoration(
@@ -47,60 +138,35 @@ class DiceCollection extends StatelessWidget {
           Wrap(
             runSpacing: 8.0,
             spacing: 8.0,
-            children: [
-              CupertinoButton(
-                  color: CupertinoColors.systemPink,
-                  padding: const EdgeInsets.all(0.0),
-                  child: const Text(
-                    'd4',
-                  ),
-                  onPressed: () {}),
-              CupertinoButton(
-                  color: CupertinoColors.systemPink,
-                  padding: const EdgeInsets.all(0.0),
-                  child: const Text(
-                    'd6',
-                  ),
-                  onPressed: () {}),
-              CupertinoButton(
-                  color: CupertinoColors.systemPink,
-                  padding: const EdgeInsets.all(0.0),
-                  child: const Text(
-                    'd8',
-                  ),
-                  onPressed: () {}),
-              CupertinoButton(
-                  color: CupertinoColors.systemPink,
-                  padding: const EdgeInsets.all(0.0),
-                  child: const Text(
-                    'd10',
-                  ),
-                  onPressed: () {}),
-              CupertinoButton(
-                  color: CupertinoColors.systemPink,
-                  padding: const EdgeInsets.all(0.0),
-                  child: const Text(
-                    'd12',
-                  ),
-                  onPressed: () {}),
-              CupertinoButton(
-                  color: CupertinoColors.systemPink,
-                  padding: const EdgeInsets.all(0.0),
-                  child: const Text(
-                    'd20',
-                  ),
-                  onPressed: () {}),
-              CupertinoButton(
-                  color: CupertinoColors.systemPink,
-                  padding: const EdgeInsets.all(0.0),
-                  child: const Text(
-                    'd100',
-                  ),
-                  onPressed: () {}),
-            ],
+            children: [...diceButtons],
           ),
         ],
       ),
     );
+  }
+}
+
+class DiceButton extends StatelessWidget {
+  const DiceButton({
+    super.key,
+    required this.dieType,
+  });
+
+  final DieType dieType;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton(
+        color: CupertinoColors.systemPink,
+        padding: const EdgeInsets.all(0.0),
+        child: Text(
+          dieType.label,
+          style: const TextStyle(
+            color: CupertinoColors.white,
+          ),
+        ),
+        onPressed: () {
+          int roll = dieType.roll();
+        });
   }
 }
