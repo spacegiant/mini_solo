ba6b623ebcb4b9407d0ab6141303c80da3b3fad4
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-04-05 21:03:07 +0100

    Add content to the New Person view

--- lib/views/new/new_person.dart
+++ lib/views/new/new_person.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import '../../utilities/app_state.dart';
+import '../../utilities/get_random_result.dart';
+import '../../utilities/update_journal.dart';
 import '../../widgets/list_button.dart';
 import '../../widgets/menu_spacer.dart';
 import '../../widgets/speech_bubble/bubble_text.dart';
 import '../../widgets/speech_bubble/speech_bubble.dart';
 import '../../widgets/view_wrapper.dart';
+import '../journal_view.dart';
 
 class NewPersonMenu extends StatefulWidget {
   const NewPersonMenu({super.key});
@@ -20,10 +23,27 @@ class _NewPersonMenuState extends State<NewPersonMenu> {
   String? line2;
   String? line3;
 
+  void updateState(ReturnObject result) {
+    setState(() {
+      line1 = result.line1;
+      line2 = result.line2;
+      line3 = null;
+    });
+  }
+
   @override
   Consumer build(BuildContext context) {
     return Consumer<AppState>(
       builder: (BuildContext context, AppState appState, Widget? child) {
+        handleUpdateBubble(
+          AppState appState,
+          ReturnObject result,
+          String label,
+        ) {
+          updateState(result);
+          updateJournal(appState, result, label);
+        }
+
         return ViewWrapper(children: [
           // TODO: Make this like a character sheet or add objects as rolled
           SpeechBubble(
@@ -44,6 +64,138 @@ class _NewPersonMenuState extends State<NewPersonMenu> {
               onPressed: () {
                 print('Pressed');
               }),
+          const Text('Character Details'),
+          ListButton(
+            label: 'Character',
+            onPressed: () {
+              getRandomResult(
+                appState: appState,
+                label: 'Mythic - Character',
+                jsonPath: 'mythic_elements/characters.json',
+                table1: 'table',
+                table2: 'table',
+                onResult: handleUpdateBubble,
+              );
+            },
+          ),
+          ListButton(
+            label: 'Appearance',
+            onPressed: () {
+              getRandomResult(
+                appState: appState,
+                label: 'Mythic - Character Appearance',
+                jsonPath: 'mythic_elements/characters_appearance.json',
+                table1: 'table',
+                table2: 'table',
+                onResult: handleUpdateBubble,
+              );
+            },
+          ),
+          ListButton(
+            label: 'Background',
+            onPressed: () {
+              getRandomResult(
+                appState: appState,
+                label: 'Mythic - Character Background',
+                jsonPath: 'mythic_elements/characters_background.json',
+                table1: 'table',
+                table2: 'table',
+                onResult: handleUpdateBubble,
+              );
+            },
+          ),
+          ListButton(
+            label: 'Conversation',
+            onPressed: () {
+              getRandomResult(
+                appState: appState,
+                label: 'Mythic - Character Conversation',
+                jsonPath: 'mythic_elements/characters_conversations.json',
+                table1: 'table',
+                table2: 'table',
+                onResult: handleUpdateBubble,
+              );
+            },
+          ),
+          ListButton(
+            label: 'Descriptors',
+            onPressed: () {
+              getRandomResult(
+                appState: appState,
+                label: 'Mythic - Character Descriptors',
+                jsonPath: 'mythic_elements/characters_descriptors.json',
+                table1: 'table',
+                table2: 'table',
+                onResult: handleUpdateBubble,
+              );
+            },
+          ),
+          ListButton(
+            label: 'Identity',
+            onPressed: () {
+              getRandomResult(
+                appState: appState,
+                label: 'Mythic - Character Identity',
+                jsonPath: 'mythic_elements/characters_identity.json',
+                table1: 'table',
+                table2: 'table',
+                onResult: handleUpdateBubble,
+              );
+            },
+          ),
+          ListButton(
+            label: 'Motivation',
+            onPressed: () {
+              getRandomResult(
+                appState: appState,
+                label: 'Mythic - Character Motivation',
+                jsonPath: 'mythic_elements/characters_motivations.json',
+                table1: 'table',
+                table2: 'table',
+                onResult: handleUpdateBubble,
+              );
+            },
+          ),
+          ListButton(
+            label: 'Personality',
+            onPressed: () {
+              getRandomResult(
+                appState: appState,
+                label: 'Mythic - Character Personality',
+                jsonPath: 'mythic_elements/characters_personality.json',
+                table1: 'table',
+                table2: 'table',
+                onResult: handleUpdateBubble,
+              );
+            },
+          ),
+          ListButton(
+            label: 'Skills',
+            onPressed: () {
+              getRandomResult(
+                appState: appState,
+                label: 'Mythic - Character Skills',
+                jsonPath: 'mythic_elements/characters_skills.json',
+                table1: 'table',
+                table2: 'table',
+                onResult: handleUpdateBubble,
+              );
+            },
+          ),
+          ListButton(
+            label: 'Traits and Flaws',
+            onPressed: () {
+              getRandomResult(
+                appState: appState,
+                label: 'Mythic - Character Traits and Flaws',
+                jsonPath: 'mythic_elements/characters_traits_and_flaws.json',
+                table1: 'table',
+                table2: 'table',
+                onResult: handleUpdateBubble,
+              );
+            },
+          ),
+          const Text('Other'),
           if (appState.showFutureFeatures == true)
             ListButton(
                 label: 'Sex',
