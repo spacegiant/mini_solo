39170487bac3f9f5088ffa2335df5cbbb527f6f3
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-04-08 16:31:00 +0100

    Add test for chaos factor setter and getter

--- test/app_state_test.dart
+++ test/app_state_test.dart
@@ -13,21 +13,6 @@ void main() {
       expect(data.campaignData?.mythic.chaosFactor, 5);
     });
 
-    test('get showFutureFeatures returns bool', () {
-      final data = AppState();
-      CampaignData campaignData = initCampaignDataData('test campaign name');
-      data.setCampaignData(campaignData);
-      expect(data.campaignData?.settings.general.showFutureSettings, false);
-    });
-
-    test('toggleShowFutureFeatures toggles bool', () {
-      final data = AppState();
-      CampaignData campaignData = initCampaignDataData('test campaign name');
-      data.setCampaignData(campaignData);
-      data.toggleShowFutureFeatures();
-      expect(data.campaignData?.settings.general.showFutureSettings, true);
-    });
-
     test('setSaveCallback is called with saveCampaignDataToDisk', () {
       MockCallback mock = MockCallback();
       final data = AppState();
@@ -38,6 +23,70 @@ void main() {
       });
       data.saveCampaignDataToDisk();
       expect(mock.called(1), isTrue);
+    });
+
+    group('General settings', () {
+      test('get showFutureFeatures returns bool', () {
+        final data = AppState();
+        CampaignData campaignData = initCampaignDataData('test campaign name');
+        data.setCampaignData(campaignData);
+        expect(data.campaignData?.settings.general.showFutureSettings, false);
+      });
+
+      test('toggleShowFutureFeatures toggles bool', () {
+        final data = AppState();
+        CampaignData campaignData = initCampaignDataData('test campaign name');
+        data.setCampaignData(campaignData);
+        data.toggleShowFutureFeatures();
+        expect(data.campaignData?.settings.general.showFutureSettings, true);
+      });
+    });
+
+    group('Chaos Factor', () {
+      test('getter chaosFactor returns value', () {
+        MockCallback mock = MockCallback();
+        final data = AppState();
+        CampaignData campaignData = initCampaignDataData('test campaign name');
+        data.setCampaignData(campaignData);
+
+        expect(data.chaosFactor, 5);
+      });
+
+      test('increaseChaosFactor increases CF', () {
+        MockCallback mock = MockCallback();
+        final data = AppState();
+        CampaignData campaignData = initCampaignDataData('test campaign name');
+        data.setCampaignData(campaignData);
+
+        data.increaseChaosFactor();
+
+        expect(data.chaosFactor, 6);
+      });
+
+      test('decreaseChaosFactor decreases CF', () {
+        MockCallback mock = MockCallback();
+        final data = AppState();
+        CampaignData campaignData = initCampaignDataData('test campaign name');
+        data.setCampaignData(campaignData);
+
+        data.decreaseChaosFactor();
+
+        expect(data.chaosFactor, 4);
+      });
+
+      test('resetChaosFactor resets CF', () {
+        MockCallback mock = MockCallback();
+        final data = AppState();
+        CampaignData campaignData = initCampaignDataData('test campaign name');
+        data.setCampaignData(campaignData);
+
+        data.decreaseChaosFactor();
+        data.decreaseChaosFactor();
+        data.decreaseChaosFactor();
+        expect(data.chaosFactor, 2);
+        data.resetChaosFactor();
+        expect(data.chaosFactor, 5);
+      });
     });
   });
 }
