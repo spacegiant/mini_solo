8ad48846bbcfb07d34fcbf554d333898d78efb3f
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-04-08 16:43:53 +0100

    Add test for saveCallbackExists

--- test/app_state_test.dart
+++ test/app_state_test.dart
@@ -4,89 +4,97 @@ import 'package:mini_solo/utilities/mock_callback.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Test AppState', () {
-    test('Init campaign data', () {
+  test('Init campaign data', () {
+    final data = AppState();
+    CampaignData campaignData = initCampaignDataData('test campaign name');
+    data.setCampaignData(campaignData);
+    expect(data.campaignData?.name, 'test campaign name');
+    expect(data.campaignData?.mythic.chaosFactor, 5);
+  });
+
+  test('saveCallbackExists', () {
+    final data = AppState();
+    CampaignData campaignData = initCampaignDataData('test campaign name');
+    data.setCampaignData(campaignData);
+
+    expect(data.saveCallbackExists, isFalse);
+    data.setSaveCallback((data) {});
+    expect(data.saveCallbackExists, isTrue);
+  });
+
+  test('setSaveCallback is called with saveCampaignDataToDisk', () {
+    MockCallback mock = MockCallback();
+    final data = AppState();
+    CampaignData campaignData = initCampaignDataData('test campaign name');
+    data.setCampaignData(campaignData);
+    data.setSaveCallback((data) {
+      mock.call();
+    });
+    data.saveCampaignDataToDisk();
+    expect(mock.called(1), isTrue);
+  });
+
+  group('General settings', () {
+    test('get showFutureFeatures returns bool', () {
       final data = AppState();
       CampaignData campaignData = initCampaignDataData('test campaign name');
       data.setCampaignData(campaignData);
-      expect(data.campaignData?.name, 'test campaign name');
-      expect(data.campaignData?.mythic.chaosFactor, 5);
+      expect(data.campaignData?.settings.general.showFutureSettings, isFalse);
     });
 
-    test('setSaveCallback is called with saveCampaignDataToDisk', () {
+    test('toggleShowFutureFeatures toggles bool', () {
+      final data = AppState();
+      CampaignData campaignData = initCampaignDataData('test campaign name');
+      data.setCampaignData(campaignData);
+      data.toggleShowFutureFeatures();
+      expect(data.campaignData?.settings.general.showFutureSettings, isTrue);
+    });
+  });
+
+  group('Chaos Factor', () {
+    test('getter chaosFactor returns value', () {
       MockCallback mock = MockCallback();
       final data = AppState();
       CampaignData campaignData = initCampaignDataData('test campaign name');
       data.setCampaignData(campaignData);
-      data.setSaveCallback((data) {
-        mock.call();
-      });
-      data.saveCampaignDataToDisk();
-      expect(mock.called(1), isTrue);
+
+      expect(data.chaosFactor, 5);
     });
 
-    group('General settings', () {
-      test('get showFutureFeatures returns bool', () {
-        final data = AppState();
-        CampaignData campaignData = initCampaignDataData('test campaign name');
-        data.setCampaignData(campaignData);
-        expect(data.campaignData?.settings.general.showFutureSettings, false);
-      });
+    test('increaseChaosFactor increases CF', () {
+      MockCallback mock = MockCallback();
+      final data = AppState();
+      CampaignData campaignData = initCampaignDataData('test campaign name');
+      data.setCampaignData(campaignData);
 
-      test('toggleShowFutureFeatures toggles bool', () {
-        final data = AppState();
-        CampaignData campaignData = initCampaignDataData('test campaign name');
-        data.setCampaignData(campaignData);
-        data.toggleShowFutureFeatures();
-        expect(data.campaignData?.settings.general.showFutureSettings, true);
-      });
+      data.increaseChaosFactor();
+
+      expect(data.chaosFactor, 6);
     });
 
-    group('Chaos Factor', () {
-      test('getter chaosFactor returns value', () {
-        MockCallback mock = MockCallback();
-        final data = AppState();
-        CampaignData campaignData = initCampaignDataData('test campaign name');
-        data.setCampaignData(campaignData);
+    test('decreaseChaosFactor decreases CF', () {
+      MockCallback mock = MockCallback();
+      final data = AppState();
+      CampaignData campaignData = initCampaignDataData('test campaign name');
+      data.setCampaignData(campaignData);
 
-        expect(data.chaosFactor, 5);
-      });
+      data.decreaseChaosFactor();
 
-      test('increaseChaosFactor increases CF', () {
-        MockCallback mock = MockCallback();
-        final data = AppState();
-        CampaignData campaignData = initCampaignDataData('test campaign name');
-        data.setCampaignData(campaignData);
+      expect(data.chaosFactor, 4);
+    });
 
-        data.increaseChaosFactor();
+    test('resetChaosFactor resets CF', () {
+      MockCallback mock = MockCallback();
+      final data = AppState();
+      CampaignData campaignData = initCampaignDataData('test campaign name');
+      data.setCampaignData(campaignData);
 
-        expect(data.chaosFactor, 6);
-      });
-
-      test('decreaseChaosFactor decreases CF', () {
-        MockCallback mock = MockCallback();
-        final data = AppState();
-        CampaignData campaignData = initCampaignDataData('test campaign name');
-        data.setCampaignData(campaignData);
-
-        data.decreaseChaosFactor();
-
-        expect(data.chaosFactor, 4);
-      });
-
-      test('resetChaosFactor resets CF', () {
-        MockCallback mock = MockCallback();
-        final data = AppState();
-        CampaignData campaignData = initCampaignDataData('test campaign name');
-        data.setCampaignData(campaignData);
-
-        data.decreaseChaosFactor();
-        data.decreaseChaosFactor();
-        data.decreaseChaosFactor();
-        expect(data.chaosFactor, 2);
-        data.resetChaosFactor();
-        expect(data.chaosFactor, 5);
-      });
+      data.decreaseChaosFactor();
+      data.decreaseChaosFactor();
+      data.decreaseChaosFactor();
+      expect(data.chaosFactor, 2);
+      data.resetChaosFactor();
+      expect(data.chaosFactor, 5);
     });
   });
 }
