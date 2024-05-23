5f62d30c1c05e4af2c087e70c1b976ff44d2c7d0
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-05-23 15:48:20 +0100

    Tidy up

--- lib/views/dice/dice_collection.dart
+++ lib/views/dice/dice_collection.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-
 import '../../data/app_state.dart';
 import '../../data/campaign_data.dart';
 import 'dice.dart';
@@ -10,15 +9,11 @@ class DiceCollection extends StatelessWidget {
     super.key,
     required this.diceSet,
     required this.onPressed,
-    // required this.onSubmit,
-    // required this.onClear,
     required this.appState,
   });
 
   final DiceSet diceSet;
   final void Function(List<DiceRoll>) onPressed;
-  // final void Function() onSubmit;
-  // final void Function() onClear;
   final AppState appState;
 
   @override
@@ -36,61 +31,7 @@ class DiceCollection extends StatelessWidget {
       spacing: 8.0,
       children: [
         ...diceButtons,
-        // SubmitButton(onSubmit: onSubmit),
-        // ClearButton(onClear: onClear),
       ],
-    );
-  }
-}
-
-class ClearButton extends StatelessWidget {
-  const ClearButton({
-    super.key,
-    required this.onClear,
-  });
-
-  final void Function() onClear;
-
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoButton(
-      color: CupertinoColors.black,
-      padding: const EdgeInsets.all(0.0),
-      child: const Text(
-        'Clear',
-        style: TextStyle(
-          color: CupertinoColors.white,
-        ),
-      ),
-      onPressed: () {
-        onClear();
-      },
-    );
-  }
-}
-
-class SubmitButton extends StatelessWidget {
-  const SubmitButton({
-    super.key,
-    required this.onSubmit,
-  });
-
-  final void Function() onSubmit;
-
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoButton(
-      color: CupertinoColors.black,
-      padding: const EdgeInsets.all(0.0),
-      child: const Text(
-        '>',
-        style: TextStyle(
-          color: CupertinoColors.white,
-        ),
-      ),
-      onPressed: () {
-        onSubmit();
-      },
     );
   }
 }
