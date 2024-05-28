ebca71857e03c581bd859071dd07da7a117d4e73
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-05-28 10:50:51 +0100

    Add text area with controller, scrollable and expands to height.

--- lib/features/scratchpad/scratchpad_view.dart
+++ lib/features/scratchpad/scratchpad_view.dart
@@ -4,17 +4,37 @@ import 'package:provider/provider.dart';
 import '../../data/app_state.dart';
 
 class ScratchpadView extends StatefulWidget {
-  const ScratchpadView({super.key});
+  const ScratchpadView({
+    super.key,
+  });
 
   @override
   State<ScratchpadView> createState() => _ScratchpadViewState();
 }
 
 class _ScratchpadViewState extends State<ScratchpadView> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<AppState>(builder: (context, appState, child) {
-      return const Placeholder();
-    });
+    return CupertinoTextField(
+      placeholder: '...',
+      controller: _controller,
+      expands: true,
+      minLines: null,
+      maxLines: null,
+    );
   }
 }
