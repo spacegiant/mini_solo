668779276f2751c2b24bf79678c68218d2ee70ee
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-05-30 17:03:06 +0100

    Switch edit random table popup to ListView builder

--- lib/widgets/popups/edit_random_table_popup.dart
+++ lib/widgets/popups/edit_random_table_popup.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mini_solo/constants.dart';
 import '../../data/app_settings_data.dart';
 import '../../data/app_state.dart';
+import '../gap.dart';
 
 class EditRandomTable extends StatelessWidget {
   final AppState appState;
@@ -20,32 +23,32 @@ class EditRandomTable extends StatelessWidget {
     String? detail = entry.title;
 
     List<RandomTableRow> rows = entry.rows;
+    int recordCount = rows.length;
 
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
-          Text(detail),
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                ...rows.map(
-                  (row) => Row(
-                    children: [
-                      Text(row.weight.toString()),
-                      Expanded(child: Text(row.label)),
-                      CupertinoButton(
-                          child: const Text('Delete'), onPressed: () {})
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
+          Text('$detail ($recordCount entries)'),
+          const Divider(),
+          SizedBox(
+              height: 400.0,
+              child: ListView.builder(
+                itemCount: recordCount,
+                prototypeItem: CupertinoListTile(title: Text(rows.first.label)),
+                itemBuilder: (context, index) {
+                  return ListViewWidget(
+                    row: rows[index],
+                  );
+                },
+              )),
+          const Divider(),
           Row(
             children: [
               CupertinoButton(
-                  child: const Text('Delete Table'), onPressed: () {}),
+                  color: kWarningColour,
+                  child: const Text('Delete Table'),
+                  onPressed: () {}),
               CupertinoButton(child: const Text('Export'), onPressed: () {}),
             ],
           ),
@@ -64,6 +67,32 @@ class EditRandomTable extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Text(detail),
+      ),
+    );
+  }
+}
+
+class ListViewWidget extends StatelessWidget {
+  const ListViewWidget({
+    super.key,
+    required this.row,
+  });
+
+  final RandomTableRow row;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoListTile(
+      onTap: () {},
+      title: Row(
+        children: [
+          Text(row.weight.toString()),
+          const Gap(),
+          Expanded(child: Text(row.label)),
+          CupertinoButton(
+              child: const Icon(CupertinoIcons.trash, size: 12.0),
+              onPressed: () {})
+        ],
       ),
     );
   }
