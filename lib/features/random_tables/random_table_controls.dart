7591a4efadbac2b08152d508dcbe43a69005562b
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-05-30 13:19:26 +0100

    Refactor random tables to have rows with chance and potential link to another random table

--- lib/features/random_tables/random_table_controls.dart
+++ lib/features/random_tables/random_table_controls.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:mini_solo/data/app_settings_data.dart';
 import 'package:mini_solo/widgets/list_button.dart';
@@ -22,8 +24,9 @@ class RandomTables extends StatelessWidget {
         ...randomTables.map((table) => ListButton(
             label: table.title,
             onPressed: () {
-              String test = rollTable();
+              String test = rollTable(table).resultString;
               print(test);
+              // TODO: Send to JOurnal
             })),
         CupertinoButton(
             child: const Icon(
@@ -39,6 +42,46 @@ class RandomTables extends StatelessWidget {
   }
 }
 
-String rollTable() {
-  return 'test';
+RollTableResult rollTable(RandomTableEntry table) {
+  int weightsSum = 0;
+  int tally = 0;
+  RollTableResult result = RollTableResult(
+      randomRoll: 0, resultString: '', lowerBounds: 0, upperBounds: 0);
+  var rows = table.rows;
+
+  for (int i = 0; i < rows.length; i++) {
+    weightsSum += rows[i].chance!;
+  }
+
+  int randomRoll = Random().nextInt(weightsSum);
+
+  for (int i = 0; i < rows.length; i++) {
+    tally += rows[i].chance!;
+
+    if (randomRoll < tally) {
+      result = RollTableResult(
+          randomRoll: randomRoll,
+          resultString: rows[i].title,
+          lowerBounds: tally,
+          upperBounds: rows[i].chance ?? 0);
+
+      break;
+    }
+  }
+
+  return result;
+}
+
+class RollTableResult {
+  int randomRoll;
+  String resultString;
+  int lowerBounds;
+  int upperBounds;
+
+  RollTableResult({
+    required this.randomRoll,
+    required this.resultString,
+    required this.lowerBounds,
+    required this.upperBounds,
+  });
 }
