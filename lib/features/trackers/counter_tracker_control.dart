8d57c3aa6a70473fd10719471526c941dd2852a8
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-06-12 19:46:19 +0100

    Add increase decrease buttons to counter tracker control

--- lib/features/trackers/counter_tracker_control.dart
+++ lib/features/trackers/counter_tracker_control.dart
@@ -3,6 +3,7 @@ import 'package:mini_solo/features/trackers/tracker_container.dart';
 
 import '../../data/app_state.dart';
 import '../../data/campaign_data.dart';
+import '../../widgets/gap.dart';
 
 class CounterWidget extends StatelessWidget {
   const CounterWidget({
@@ -16,15 +17,46 @@ class CounterWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TrackerContainer(
-        minWidth: 160.0,
-        appState: appState,
-        id: entry.id,
-        child: Column(
-          children: [
-            Text(entry.label),
-            Text(entry.currentValue.toString()),
-          ],
-        ));
+    void handleTap({int modifier = 1}) {
+      appState.updateTrackerEntry(
+        entry.id,
+        TrackerEntry(
+          label: entry.label,
+          currentValue: entry.currentValue + modifier,
+          trackerType: entry.trackerType,
+        ),
+      );
+    }
+
+    return Stack(children: [
+      TrackerContainer(
+          onTap: () {
+            handleTap(modifier: -1);
+          },
+          onTapRight: () {
+            handleTap();
+          },
+          minWidth: 160.0,
+          appState: appState,
+          id: entry.id,
+          child: Column(
+            children: [
+              Text(entry.label),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(CupertinoIcons.arrowtriangle_left_fill),
+                  SizedBox(
+                      width: 100.0,
+                      child: Text(
+                        entry.currentValue.toString(),
+                        textAlign: TextAlign.center,
+                      )),
+                  Icon(CupertinoIcons.arrowtriangle_right_fill),
+                ],
+              ),
+            ],
+          )),
+    ]);
   }
 }
