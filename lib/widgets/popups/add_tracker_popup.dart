43b8e489326527904cc6b6fdc8e691957be9c3cd
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-06-25 12:56:37 +0100

    Move trackerOptionButton to own file

--- lib/widgets/popups/add_tracker_popup.dart
+++ lib/widgets/popups/add_tracker_popup.dart
@@ -11,6 +11,7 @@ import '../../data/campaign_data.dart';
 import '../../features/trackers/tracker_options.dart';
 import '../../svg_icon.dart';
 import '../gap.dart';
+import '../trackers/trackerOptionButton.dart';
 
 class AddTrackerPopup extends StatefulWidget {
   const AddTrackerPopup({
@@ -187,49 +188,6 @@ class _AddTrackerPopupState extends State<AddTrackerPopup> {
     setState(() {
       _maxValueController.text = value;
     });
-  }
-
-  Widget trackerOptionButton({
-    required List<SVGIcon> images,
-    String? label,
-    double? size,
-    required String id,
-    required String selectedId,
-    required Function(String) onSelect,
-  }) {
-    Color buttonColor = CupertinoColors.extraLightBackgroundGray;
-    Color textColor = CupertinoColors.black;
-
-    if (selectedId == id) {
-      buttonColor = CupertinoColors.systemYellow;
-    }
-
-    List<SvgIcon> svgIcons = images
-        .map((image) => SvgIcon(
-              icon: image,
-              height: 36.0,
-            ))
-        .toList();
-
-    return CupertinoButton(
-      onPressed: () {
-        onSelect(id);
-      },
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      color: buttonColor,
-      child: Wrap(
-        crossAxisAlignment: WrapCrossAlignment.center,
-        children: [
-          ...svgIcons,
-          if (label != null) const Gap(),
-          if (label != null)
-            Text(
-              label,
-              style: TextStyle(color: textColor),
-            ),
-        ],
-      ),
-    );
   }
 
   Row buttonBar() {
