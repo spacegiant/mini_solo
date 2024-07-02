df481e1da22677c4328a4f8e38d9047dc14b22b5
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-07-02 11:36:19 +0100

    Refactor label and input widget to handle vertical and horizontal layouts

--- lib/widgets/label_and_input.dart
+++ lib/widgets/label_and_input.dart
@@ -6,18 +6,28 @@ class LabelAndInput extends StatelessWidget {
   const LabelAndInput({
     super.key,
     required this.label,
-    this.enabled,
+    this.enabled = true,
     required this.controller,
     required this.onChanged,
+    this.axis = Axis.vertical,
   });
 
   final String label;
   final bool? enabled;
   final TextEditingController controller;
   final void Function(String) onChanged;
+  final Axis axis;
 
   @override
   Widget build(BuildContext context) {
+    if (axis == Axis.vertical) {
+      return verticalLayout();
+    } else {
+      return horizontalLayout();
+    }
+  }
+
+  Row horizontalLayout() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
@@ -29,6 +39,20 @@ class LabelAndInput extends StatelessWidget {
             controller: controller,
             enabled: enabled ?? false,
           ),
+        ),
+      ],
+    );
+  }
+
+  Column verticalLayout() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(label),
+        CupertinoTextField(
+          onChanged: onChanged,
+          controller: controller,
+          enabled: enabled ?? false,
         ),
       ],
     );
