d25f4a3d4f2e050d53352907c5a9d5b995025451
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-07-05 15:01:25 +0100

    move initialise groups to own file

--- lib/data/campaign_data.dart
+++ lib/data/campaign_data.dart
@@ -1,4 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:mini_solo/data/initialise_groups.dart';
 import 'package:mini_solo/utilities/string/convert_to_filename.dart';
 import '../features/grouping/group.dart';
 import '../features/kard/kard.dart';
@@ -210,57 +211,7 @@ CampaignData initCampaignDataData(String campaignName) {
     tracker: [],
     newScene: [],
     kards: [],
-    groups: [
-      Group(
-        isAppGroup: false,
-        groupId: 'unsorted',
-        label: 'Unsorted',
-        controls: [],
-        color: 0xFF512500,
-      ),
-      Group(
-        isAppGroup: true,
-        groupId: 'group-mythic-fate-chart',
-        label: 'Mythic Fate Chart',
-        controls: initialMythicFateChartIds,
-        color: 0xFF7D1D3F,
-      ),
-      Group(
-        isAppGroup: true,
-        groupId: 'group-mythic-gme',
-        label: 'Mythic GME',
-        controls: initialMythicGMEIds,
-        color: 0xFF827191,
-      ),
-      Group(
-        isAppGroup: false,
-        groupId: 'group-random-tables',
-        label: 'Random Tables',
-        controls: [],
-        color: 0xFF84ACCE,
-      ),
-      Group(
-        isAppGroup: false,
-        groupId: 'group-trackers',
-        label: 'Trackers',
-        controls: [],
-        color: 0xFFD7D9B1,
-      ),
-      Group(
-        isAppGroup: true,
-        groupId: 'new-items',
-        label: 'New Items',
-        controls: initialNewItemControlIds,
-        color: 0xFFFB8F67,
-      ),
-      Group(
-        isAppGroup: true,
-        groupId: 'group-action-lists',
-        label: 'Action Lists',
-        controls: [],
-        color: 0xFFFB8F67,
-      ),
-    ],
+    groups: initialiseGroups,
   );
 }
 
---
+++ lib/data/initialise_groups.dart
@@ -0,0 +1,56 @@
+import '../features/grouping/group.dart';
+import '../views/mythic/fate_question.dart';
+import 'campaign_data.dart';
+import 'initial_new_item_controls.dart';
+
+var initialiseGroups = [
+  Group(
+    isAppGroup: false,
+    groupId: 'unsorted',
+    label: 'Unsorted',
+    controls: [],
+    color: 0xFF512500,
+  ),
+  Group(
+    isAppGroup: true,
+    groupId: 'group-mythic-fate-chart',
+    label: 'Mythic Fate Chart',
+    controls: initialMythicFateChartIds,
+    color: 0xFF7D1D3F,
+  ),
+  Group(
+    isAppGroup: true,
+    groupId: 'group-mythic-gme',
+    label: 'Mythic GME',
+    controls: initialMythicGMEIds,
+    color: 0xFF827191,
+  ),
+  Group(
+    isAppGroup: false,
+    groupId: 'group-random-tables',
+    label: 'Random Tables',
+    controls: [],
+    color: 0xFF84ACCE,
+  ),
+  Group(
+    isAppGroup: false,
+    groupId: 'group-trackers',
+    label: 'Trackers',
+    controls: [],
+    color: 0xFFD7D9B1,
+  ),
+  Group(
+    isAppGroup: true,
+    groupId: 'new-items',
+    label: 'New Items',
+    controls: initialNewItemControlIds,
+    color: 0xFFFB8F67,
+  ),
+  Group(
+    isAppGroup: true,
+    groupId: 'group-action-lists',
+    label: 'Action Lists',
+    controls: [],
+    color: 0xFFFB8F67,
+  ),
+];
