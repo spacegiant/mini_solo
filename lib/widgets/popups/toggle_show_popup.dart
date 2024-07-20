bebb07e81421a0d3d98743e044fba8370c9c9451
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-07-20 10:11:26 +0100

    Resize popup to account for keyboard

--- lib/widgets/popups/toggle_show_popup.dart
+++ lib/widgets/popups/toggle_show_popup.dart
@@ -6,18 +6,24 @@ void toggleShowPopup({
   double maxWidth = 240.0,
   double maxHeight = 180.0,
 }) {
-  showCupertinoModalPopup(
+  showCupertinoDialog(
     context: context,
+    barrierDismissible: true,
     builder: (BuildContext context) => SafeArea(
-      child: ConstrainedBox(
-        constraints: BoxConstraints(
-          maxWidth: maxWidth,
-          maxHeight: maxHeight,
-        ),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(
+            16.0, 16.0, 16.0, MediaQuery.of(context).viewInsets.bottom),
         child: CupertinoPopupSurface(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: child,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxWidth: maxWidth,
+                    maxHeight: maxHeight,
+                  ),
+                  child: child),
+            ),
           ),
         ),
       ),
